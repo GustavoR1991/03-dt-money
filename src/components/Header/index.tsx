1aef1f2ddde550df288e1fbef173e58b1b1ef9d9
GustavoR1991 <gugarosa110901@gmail.com>
2025-10-27 14:55:22 -0400

    feat: implement NewTransactionModal with Radix UI dialog and update Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,23 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 
+import * as Dialog from '@radix-ui/react-dialog'
+
 // @ts-ignore: SVG module declaration missing
 import logoImg from '../../assets/logo.svg';
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} alt="Logo" />
-        <NewTransactionButton>Nova Transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova Transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
 
