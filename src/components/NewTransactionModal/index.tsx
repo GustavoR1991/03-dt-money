0bcbf43c2d400e8eee5bcc79fab015193eec77de
GustavoR1991 <gugarosa110901@gmail.com>
2025-10-27 14:57:02 -0400

    style: format code for consistency and readability in NewTransactionModal component

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,12 @@
-import * as Dialog from '@radix-ui/react-dialog'
-import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles'
-import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
-
+import * as Dialog from "@radix-ui/react-dialog";
+import {
+  CloseButton,
+  Content,
+  Overlay,
+  TransactionType,
+  TransactionTypeButton,
+} from "./styles";
+import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
 
 export function NewTransactionModal() {
   return (
@@ -9,14 +14,14 @@ export function NewTransactionModal() {
       <Overlay />
       <Content>
         <Dialog.Title>Nova Transação</Dialog.Title>
-        <CloseButton >
+        <CloseButton>
           <X size={24} />
         </CloseButton>
 
         <form action="">
-          <input type="text" placeholder='Descrição ' required />
-          <input type="number" placeholder='Preço ' required />
-          <input type="text" placeholder='Categoria ' required />
+          <input type="text" placeholder="Descrição " required />
+          <input type="number" placeholder="Preço " required />
+          <input type="text" placeholder="Categoria " required />
 
           <TransactionType>
             <TransactionTypeButton variant="income">
@@ -27,12 +32,11 @@ export function NewTransactionModal() {
               <ArrowCircleDown size={24} />
               Saída
             </TransactionTypeButton>
-
           </TransactionType>
 
           <button type="submit">Cadastrar</button>
         </form>
       </Content>
     </Dialog.Portal>
-  )
+  );
 }
